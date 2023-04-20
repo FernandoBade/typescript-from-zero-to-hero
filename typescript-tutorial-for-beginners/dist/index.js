dd61eb3ef746da305e98596f3674a06d7d8159dd
Fernando Bade <bade.fernando@gmail.com>
2023-04-19 21:08:34 -0300

    19-04-2023

---
+++ typescript-tutorial-for-beginners/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+let age;
+age = 20;
+if (age < 50) {
+    age += 10;
+}
+age += 10;
+console.log(age);
+//# sourceMappingURL=index.js.map
---
+++ typescript-tutorial-for-beginners/src/index.ts
@@ -0,0 +1,17 @@
+// console.log("Hello World!");
+
+// var age: number = 20;
+
+// age = 36;
+
+
+let age: number;
+
+age = 20;
+if (age < 50) {
+    age += 10;
+}
+age += 10;
+
+console.log(age);
+
